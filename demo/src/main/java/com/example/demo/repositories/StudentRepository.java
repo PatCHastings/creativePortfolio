05d0febb167162733ebbe8e5e1474fc5a2e4b584
PatCHastings <phastings82@gmail.com>
2022-08-19 10:45:17 -0400

    added css parralax

--- demo/src/main/java/com/example/demo/repositories/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.model.Student;
-import org.springframework.data.repository.CrudRepository;
-// This is basically it for repositories. they are never super big
-public interface StudentRepository extends CrudRepository<Student, Long> {
-}
