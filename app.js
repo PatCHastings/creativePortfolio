1d52ca908af71b387c562592348293145aa1f7ab
PatCHastings <phastings82@gmail.com>
2022-08-21 11:48:52 -0400

    added moon click event, css pointer events

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-const moonEl = document.querySelector(".moon");
+const fullmoonEl = document.querySelector("#moon");
+const crescentMoonEl = document.getElementById("crescentMoon");
+
+// crescentMoonEl.style.transform = "rotate(-40deg)";
+
+
+let moonButton = document.getElementById('moon'); 
+moonButton.addEventListener("click", ()=> {
+    console.log("hello");
+    document.querySelector("#moon").style.visibility="hidden";  
+    document.querySelector("#crescentMoon").style.visibility="visible";  
+    if (document.querySelector("#moon").style.visibility="hidden") {
+        let crescentmoonButton = document.getElementById('crescentMoon'); 
+        crescentmoonButton.addEventListener("click", ()=> {
+            console.log("ello gov");
+            document.querySelector("#moon").style.visibility="visible";  
+            document.querySelector("#crescentMoon").style.visibility="hidden";  
+        })
+    }
+})
+
+
+
+
