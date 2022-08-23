4d754fd5d3ebb0cbfaf3813048c8560f55015098
Patrick Hastings <phastings82@gmail.com>
2022-08-23 14:35:17 -0400

    added player class

--- elements.js
+++ elements.js
@@ -1,10 +1,11 @@
-
-
 class Elements {
     constructor() {
         this._active == true; 
-    }
-}
 
+
+    }
+
+
+}
 
 export default Elements; 
---
+++ player.js
@@ -0,0 +1,25 @@
+class Player {
+constructor() {
+    this._exp = 0;
+    this._lvl = 1;
+    this._hp = 1;
+    this._mp = 1;
+    this._hud = false;
+}
+
+get _exp() {
+    return this._exp
+}
+get lvl() {
+    return this._lvl
+}
+get hp() {
+    return this._hp
+}
+get mp() {
+    return this._mp
+}
+get hud() {
+    return this._hud
+}
+}
