bd2bbd6f9d0529f46a2aac21a3e457a94f9585bf
PatCHastings <phastings82@gmail.com>
2022-08-25 00:38:11 -0400

    yes button interacts with timer

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ const fullmoonEl = document.querySelector("#moon");
 const crescentMoonEl = document.getElementById("crescentMoon");
 
 
-
+document.querySelector("#yesButton").style.visibility="hidden";  
+document.querySelector("#noButton").style.visibility="hidden";
+document.querySelector("#time").style.visibility="hidden";    
 let moonButton = document.getElementById('moon'); 
 moonButton.addEventListener("click", ()=> {
     console.log("hello");
     document.querySelector("#moon").style.visibility="hidden";  
-    document.querySelector("#crescentMoon").style.visibility="visible";  
+    document.querySelector("#crescentMoon").style.visibility="visible"; 
+    document.querySelector("#yesButton").style.visibility="visible";  
+document.querySelector("#noButton").style.visibility="visible"; 
     introBox();
     if (document.querySelector("#moon").style.visibility="hidden") {
         let crescentmoonButton = document.getElementById('crescentMoon'); 
@@ -35,10 +39,25 @@ function introBoxHide() {
     t2.to('.introTextBox', {x: '50px', ease: "power4.out", opacity: 0 })
 }
 
-document.querySelector(".introYes").textContent="hidden"; 
-let yesBtn = document.getElementById('yesTimer'); 
+
+let yesBtn = document.getElementById('yesButton'); 
+let textEl = document.getElementById('introText');
 yesBtn.addEventListener("click", ()=> {
+    textEl.innerHTML = `<p>That's great to hear. I wish you luck on your journey. Strength comes from experience
+    and you would do well to become stronger before time runs out..</p>`
     startTimer();
+    document.querySelector("#yesButton").style.visibility="hidden";
+    document.querySelector("#noButton").style.visibility="hidden";
+    document.querySelector("#time").style.visibility="visible";
+    
+})
+
+let noBtn = document.getElementById('noButton');
+let textElB = document.getElementById('introText');
+noBtn.addEventListener("click", ()=> {
+    textElB.innerHTML = `<p>Uh oh... look out!!</p>`
+    document.querySelector("#yesButton").style.visibility="hidden";
+    document.querySelector("#noButton").style.visibility="hidden";
 })
 
 
